88c68bf900c481264adf72c6dfe5a50856c335a1
yani82 <yani.iman1@gmail.com>
2021-09-04 00:21:53 -0400

    added navbar w/ styling

---
+++ src/components/NavBar.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+
+export default function NavBar() {
+    return (
+        <header className="bg-red-600">
+            <div className="container mx-auto flex justify-between">
+                <nav className="flex">
+                    <NavLink to="/" 
+                    exact 
+                    activeClassName="text-white"
+                    className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest">
+                        Yani
+                    </NavLink>
+                    <NavLink to ="/post"
+                    className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+                    activeClassName="text-red-100 bg-red-700">
+                        Blog Posts
+                    </NavLink>
+                    <NavLink to ="/project"
+                    className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+                    activeClassName="text-red-100 bg-red-700">
+                        Projects
+                    </NavLink>
+                    <NavLink to ="/about"
+                    className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+                    activeClassName="text-red-100 bg-red-700">
+                        About me
+                    </NavLink>
+                </nav>
+            </div>
+        </header>
+    )
+}
