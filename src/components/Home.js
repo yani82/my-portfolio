457adc46f00b5532d43132f4e317b1abb49a338d
yani82 <yani.iman1@gmail.com>
2021-09-06 23:48:16 -0400

    completed home and having trouble syncing post to Sanity

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
+import image from "../david-clode-PteQb2-8rmY-unsplash.jpg";
 
 export default function Home() {
     return (
         <main>
-            <img />
-            <section>
-                <h1>Hi, I'm Yani!</h1>
+            <img src={image} alt="Palm Licuala Grandis" className="absolute object-cover w-full h-full"  />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Hi, I'm Yani!</h1>
             </section>
         </main>
     )
