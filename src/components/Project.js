67fe1d4d05f64c480baa6fb9842fc1baa9224108
yani82 <yani.iman1@gmail.com>
2021-09-11 18:07:21 -0400

    building out how data is rendered in projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import project from "../../studio/schemas/project.js";
+import sanityClient from "../client.js";
 
 export default function Project() {
-    return <h1>Project Page</h1>
+    const [projectData, setProjectData] = useState(null);
+
+    useEffect(() => {
+        sanityClient.fetch(`*[_type == "project"]{
+            title,
+            date,
+            place,
+            description,
+            projectType,
+            link,
+            tags
+        }`).then((data) => setProjectData(data))
+        .catch(console.error); 
+    }, []);
+
+    return (
+        <main className="bg-green-100 min-h-screen p-12">
+            <section className="container mx-auto">
+                <h1 className="text-5xl flex justify-center cursive">Projects</h1>
+                <h2 className="text-lg text-gray-600 flex justify-center mb-12">Welcome to my projects page</h2>
+                <section className="grid grid-cols-2 gap-8">
+                    <article className="relative rounded-lg shadow-xl bg-white p-16">
+                        <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-red-700">
+                            <a 
+                            href={project.link}
+                            alt={project.title}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            >{project.title}</a>
+                        </h3>
+                        <div className="text-gray-500 text-xs space-x-4">
+                            <span>
+                                <strong className="font-bold">Finished on</strong>:{" "}
+                                {new Date(project.date).toLocaleDateString()}
+                            </span>
+                            <span>
+                                <strong className="font-bold ">Company</strong>:{" "}
+                                {project.place}
+                            </span>
+                            <span>
+                                <strong className=""></strong>
+                            </span>
+                            <p></p>
+                            <span></span>
+                        </div>
+                    </article>
+                </section>
+            </section>
+        </main>
+    ) 
 }
