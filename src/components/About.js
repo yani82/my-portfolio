ff5aacc8a364d208a160cb16d6ea3c3f723b59f0
yani82 <yani.iman1@gmail.com>
2021-09-12 01:26:13 -0400

    fixed Ruby CLI post and building about me

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import sanityClient from "../client.js";
+import flamingo from "../flamingo.jpg"; 
 
 export default function About() {
-    return <h1>About Page</h1>
+    const [author, setAuthor] = useState(null);
+
+    useEffect(() => {
+        sanityClient.fetch(`*[_type == "author"]{
+            name,
+            bio,
+            "authorImage": image.asset->url
+        }`).then((data) => setAuthor(data[0]))
+        .catch(console.error);
+    }, []);
+
+    if (!author) return <div>Loading... </div>
+
+    return (
+        <main className="relative">
+            <img src={flamingo} alt="Flamingo" className="absolute w-full" />
+            <div className="p-10 lg:pt-48 container mx-auto relative">
+                <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
+                    <img src={urlFor(author.authorImage)}  /> 
+                    <div>
+                        <h1></h1>
+                        <div>
+                            BLOCK CONTENT
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </main>
+    )
 }
