e20235ec6f2dbbdf99b5b272bbb6738869aa5bb6
yani82 <yani.iman1@gmail.com>
2021-09-16 22:33:07 -0400

    completed about pg and changed img

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from "react";
 import sanityClient from "../client.js";
-import flamingo from "../flamingo.jpg"; 
+import nyc from "../nyc.jpg"; 
+import imageUrlBuilder from "@sanity/image-url";
+import BlockContent from "@sanity/block-content-to-react";
+
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+    return builder.image(source)
+}
 
 export default function About() {
     const [author, setAuthor] = useState(null);
@@ -18,14 +25,21 @@ export default function About() {
 
     return (
         <main className="relative">
-            <img src={flamingo} alt="Flamingo" className="absolute w-full" />
+            <img src={nyc} alt="NYC" className="absolute w-full" />
             <div className="p-10 lg:pt-48 container mx-auto relative">
                 <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
-                    <img src={urlFor(author.authorImage)}  /> 
-                    <div>
-                        <h1></h1>
-                        <div>
-                            BLOCK CONTENT
+                    <img 
+                        src={urlFor(author.authorImage).url()} 
+                        className="rounded w-32 h-32 lg:w-64 lg: h-64 mr-8" 
+                        alt={author.name} 
+                    /> 
+                    <div className="text-lg flex flex-col justify-center">
+                        <h1 className="cursive text-6xl text-green-300 mb-4">
+                            Hey there! I'm {" "}
+                            <span className="text-green-100">{author.name}</span>
+                        </h1>
+                        <div className="prose lg:prose-xl text-white">
+                            <BlockContent blocks={author.bio} projectId="t7o1oari" dataset="production" />
                         </div>
                     </div>
                 </section>
