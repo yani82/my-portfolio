dfc7e1c38e600b812bd5d2a99ad134a24685bf69
yani82 <yani.iman1@gmail.com>
2021-11-30 01:15:35 -0500

    figure out how to add shadow to text

--- studio/plugins/tailwind.config.js
+++ studio/plugins/tailwind.config.js
@@ -1,3 +1,10 @@
-plugins: [
-    require('tailwindcss-textshadow')
-  ]
+module.exports = {
+  theme: {
+     textShadow: {
+        'default': '0 2px 0 #000',
+        'md': '0 2px 2px #000',
+        'h2': '0 0 3px #FF0000, 0 0 5px #0000FF',
+        'h1': '0 0 3px rgba(0, 0, 0, .8), 0 0 5px rgba(0, 0, 0, .9)',
+     }
+   }
+ }
