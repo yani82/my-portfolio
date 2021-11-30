0a0e99af55b805b8a599fef42c50114ebdc69da7
yani82 <yani.iman1@gmail.com>
2021-11-30 00:13:00 -0500

    reduce opacity of leaf graphic

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,8 @@ import image from "../david-clode-PteQb2-8rmY-unsplash.jpg";
 export default function Home() {
     return (
         <main>
-            <img src={image} alt="Palm Licuala Grandis" className="absolute object-cover w-full h-full"  />
+            <img src={image} alt="Palm Licuala Grandis" className="absolute object-cover w-full h-full" imageStyle= 
+            {{opacity:0.1}} />
             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
                 <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">I'm Yani!</h1><br /><br /> 
             </section>
