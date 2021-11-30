60cb45ee3d884104184f40999f8da7a404372979
yani82 <yani.iman1@gmail.com>
2021-11-30 01:01:53 -0500

    add Tailwind text-shadow to I'm Yani

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
             <img src={image} alt="Palm Licuala Grandis" className="absolute object-cover w-full h-full" imageStyle= 
             {{opacity:0.1}} />
             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">I'm Yani!</h1><br /><br /> 
+                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name text-shadow: 3px 3px 6px rgb(0 0 0 / 26%), 0 0 5px rgb(15 3 86 / 22%)">I'm Yani!</h1><br /><br /> 
             </section>
         </main>
 
